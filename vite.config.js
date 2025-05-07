0d70dfc53add5354ec990dbb4e4ee2b3ad92933b
Genevieve Curry <gcurry@protonmail.com>
2025-05-07 15:27:12 -0700

    Add base url to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://genevievecurry.github.io/plant-tracker/",
   build: {
     outDir: "docs", // Change output directory to 'docs'
     emptyOutDir: true, // Clear the output directory before building
