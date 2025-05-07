8f977e8e76275cdcc68a374b8e4c60dea8b6545a
Genevieve Curry <gcurry@protonmail.com>
2025-05-07 15:20:14 -0700

    Change dist to docs for GH pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "docs", // Change output directory to 'docs'
+    emptyOutDir: true, // Clear the output directory before building
+  },
 });
